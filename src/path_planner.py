ff96373fc375a483908e143234b87e52bd0b2f68
Michael Farrell <michaeldavidfarrell@gmail.com>
2016-09-28 16:06:10 -0600

    path_planner complete

--- src/path_planner.py
+++ src/path_planner.py
@@ -13,11 +13,30 @@ def talker():
 	waypointPublisher = rospy.Publisher('waypoint_path',FW_Waypoint, queue_size=10)
 	
 	Va = 8.5 # 11.0
-	wps[5*num_waypoints] =  {
+	wps =  [
                 -10, -10, -30, -45, Va,
                 -10, -125, -30, -135*math.pi/180, Va,
                 -125, -10, -30, 45*math.pi/180, Va,
-                -125, -125, -30, 135*math.pi/180, Va}
-	print(wps)
+                -125, -125, -30, 135*math.pi/180, Va]
+	for i in range(0,num_waypoints):
+		new_waypoint = FW_Waypoint()
+		
+		new_waypoint.w[0] = wps[i*5 + 0]
+		new_waypoint.w[1] = wps[i*5 + 1]
+		new_waypoint.w[2] = wps[i*5 + 2]
+		new_waypoint.chi_d = wps[i*5 + 3]
+		
+		new_waypoint.chi_valid = True # False
+		new_waypoint.Va_d = wps[i*5 + 4]
+		
+		waypointPublisher.publish(new_waypoint)
+		
+		d = rospy.Duration(0.5)
+		rospy.sleep(d)
+						
 
-talker()
+if __name__ == '__main__':
+	try:
+		talker()
+	except rospy.ROSInterruptException:
+		pass
