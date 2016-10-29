59ccce013aba299c2def42cc7ed52584859a69b8
Michael <michaeldavidfarrell@gmail.com>
2016-10-28 18:05:32 -0600

    updates to path_manager_example

--- src/path_manager_example.py
+++ src/path_manager_example.py
@@ -4,6 +4,9 @@ import path_manager_base
 import numpy as np
 import math as *
 
+M_PI_F = 3.14159265358979323846
+M_PI_2_F = 1.57079632679489661923
+
 class path_manager_example(path_manager_base):
 
 	# Init
@@ -69,11 +72,11 @@ class path_manager_example(path_manager_base):
 	def mo(self,inpt):
 		val = 0.0
 		if (in > 0):
-			val = fmod(inpt, 2*np.pi)
+			val = fmod(inpt, 2*M_PI_F)
 		else:
 			n = 0.0
-			n = floor(inpt/2/np.pi)
-			val = inpt - n*2*np.pi
+			n = floor(inpt/2/M_PI_F)
+			val = inpt - n*2*M_PI_F
 		return val
 
 	def dubinsParameters(self, start_node, end_node, R):
@@ -81,4 +84,85 @@ class path_manager_example(path_manager_base):
 		if (ell < 2*R):
 			pass
 		else:
-			self._dubinspath.ps(0) = start_node.w[0]
+			self._dubinspath.ps[0] = start_node.w[0]
+			self._dubinspath.ps[1] = start_node.w[1]
+			self._dubinspath.ps[2] = start_node.w[2]
+			self._dubinspath.chis = start_node.chi_d
+			self._dubinspath.pe[0] = end_node.w[0]
+			self._dubinspath.pe[1] = end_node.w[1]
+			self._dubinspath.pe[2] = end_node.w[2]
+			self._dubinspath.chie = end_node.chi_d
+
+			crs = _dubinspath.ps
+			crs[0] += R*(cos(M_PI_2_F)*cos(self._dubinspath.chis) - sin(M_PI_2_F)*sin(self._dubinspath.chis))
+			crs[1] += R*(sin(M_PI_2_F)*cos(self._dubinspath.chis) + cos(M_PI_2_F)*sin(self._dubinspath.chis))
+			clss = _dubinspath.ps;
+			clss[0] += R*(cos(-M_PI_2_F)*cos(self._dubinspath.chis) - sin(-M_PI_2_F)*sin(self._dubinspath.chis))
+			clss[1] += R*(sin(-M_PI_2_F)*cos(self._dubinspath.chis) + cos(-M_PI_2_F)*sin(self._dubinspath.chis))
+			cre = _dubinspath.pe;
+			cre[0] += R*(cos(M_PI_2_F)*cos(self._dubinspath.chie) - sin(M_PI_2_F)*sin(self._dubinspath.chie))
+			cre[1] += R*(sin(M_PI_2_F)*cos(self._dubinspath.chie) + cos(M_PI_2_F)*sin(self._dubinspath.chie))
+			cle = _dubinspath.pe;
+			cle[0] += R*(cos(-M_PI_2_F)*cos(self._dubinspath.chie) - sin(-M_PI_2_F)*sin(self._dubinspath.chie))
+			cle[1] += R*(sin(-M_PI_2_F)*cos(self._dubinspath.chie) + cos(-M_PI_2_F)*sin(self._dubinspath.chie))
+
+			# compute L1
+			theta = atan2(cre[1] - crs[1], cre[0] - crs[0])
+			L1 = (crs - cre).size() + R*self.mo(2*M_PI_F + self.mo(theta - M_PI_2_F) - self.mo(self._dubinspath.chis - M_PI_2_F))
+                + R*self.mo(2*M_PI_F + self.mo(self._dubinspath.chie - M_PI_2_F) - self.mo(theta - M_PI_2_F))
+
+            # compute L2
+			ell = (cle - crs).size()
+			theta = atan2(cle[1] - crs[1], cle[0] - crs[0])
+			if(2*R/ell > 1.0 || 2*R/ell < -1.0):
+				L2 = 9999.0;
+			else:
+				theta2 = theta - M_PI_2_F + asin(2*R/ell);
+				L2 = sqrt(ell*ell - 4*R*R) + R*self.mo(2*M_PI_F + self.mo(theta2) - self.mo(self._dubinspath.chis - M_PI_2_F))
+				    + R*self.mo(2*M_PI_F + self.mo(theta2 + M_PI_F) - self.mo(self._dubinspath.chie + M_PI_2_F));
+
+		    # compute L3
+			ell = (cre - clss).size()
+			theta = atan2(cre[1] - clss[1], cre[0] - clss[0])
+			if (2*R/ell > 1.0 || 2*R/ell < -1.0):
+				L3 = 9999.0
+			else:
+				theta2 = acos(2*R/ell)
+				L3 = sqrt(ell*ell - 4*R*R) + R*self.mo(2*M_PI_F + self.mo(self._dubinspath.chis + M_PI_2_F) - self.mo(theta + theta2))
+			        + R*self.mo(2*M_PI_F + self.mo(self._dubinspath.chie - M_PI_2_F) - self.mo(theta + theta2 - M_PI_F))
+
+			# compute L4
+			theta = atan2(cle[1]-clss[1],cle[0]-clss[0])
+			L4 = (clss - cle).size() + R*self.mo(2*M_PI_F + self.mo(self._dubinspath.chis + M_PI_2_F) - mo(theta + M_PI_2_F))
+			    + R*self.mo(2*M_PI_F + self.mo(theta + M_PI_2_F) - self.mo(self._dubinspath.chie + M_PI_2_F))
+
+			# L = minimum distance
+			idx = 1
+			self._dubinspath.L = L1
+			if (L2 < self._dubinspath.L):
+				self._dubinspath.L = L2
+				idx = 2
+			if (L3 < self._dubinspath.L):
+				self._dubinspath.L = L3
+				idx = 3
+			if (L4 < self._dubinspath.L):
+				self._dubinspath.L = L4
+				idx = 4
+
+			e1 = np.array([0.0, 0.0, 0.0])
+
+			# cpp = switch statement here
+			if idx == 1:
+				self._dubinspath.cs = crs;
+				self._dubinspath.lams = 1;
+				self._dubinspath.ce = cre;
+				self._dubinspath.lame = 1;
+				self._dubinspath.q1 = (cre - crs).normalized();
+				self._dubinspath.w1 = self._dubinspath.cs + (self.rotz(-M_PI_2_F)*self._dubinspath.q1)*R;
+				self._dubinspath.w2 = self._dubinspath.ce + (self.rotz(-M_PI_2_F)*self._dubinspath.q1)*R;
+			elif idx = 2:
+				###
+			elif idx = 3:
+				###
+			elif idx = 4:
+				###
